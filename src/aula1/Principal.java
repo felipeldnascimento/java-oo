a68b0e953a52596cbe8c1b4059181d1a01061619
Felipe <felipeldnascimento@gmail.com>
2022-05-30 17:46:16 -0300

    first commit

---
+++ src/aula1/Principal.java
@@ -0,0 +1,12 @@
+package aula1;
+
+public class Principal {
+
+    public static void main (String[] args) {
+        System.out.println("Aula 1");
+
+        int idade = 33;
+
+        System.out.println("Idade inserida: " + idade);
+    }
+}
