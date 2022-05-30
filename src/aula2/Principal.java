341f471e0a27d8fb76cb25a59c2f7c5e5117aed9
Felipe <felipeldnascimento@gmail.com>
2022-05-30 18:51:51 -0300

    Aula 2

---
+++ src/aula2/Principal.java
@@ -0,0 +1,27 @@
+package aula2;
+
+public class Principal {
+
+    public static void main (String[] args) {
+
+        int dia = 4;
+        int outroDia = 4 + dia;
+
+        System.out.println("Soma: " + outroDia);
+
+        int brindes = 15;
+        int pessoas = 2;
+
+        System.out.println("Divisão: " + brindes / pessoas);
+
+        double peso = 4.0;
+        int quantidade = 10;
+
+        System.out.println("Multiplicação: " + peso * quantidade);
+
+        double preco = 5.5;
+        int ingressos = 4;
+
+        System.out.println("Multplicação 2: " + preco * ingressos);
+    }
+}
